ad987860e39d0fee776481302ed514acaf1e675a
VeronicaDem <veron.dem4enko@yandex.ru>
2021-04-08 10:31:26 +0300

    add unfixed task 6

---
+++ lesson6/src/main/java/Films.java
@@ -0,0 +1,36 @@
+
+import ru.croc.models.Film;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.*;
+
+/**
+ * Фильмы.
+ */
+@XmlRootElement(name="films")
+public class Films {
+    /**
+     * Список фильмов.
+     */
+    @XmlElement(name="film")
+    List<Film> films = new ArrayList<>();
+    public Films() {
+
+    }
+    public Films(ArrayList<Film> films) {
+       this.films = films;
+    }
+    public Films(List<Film> films) {
+        this.films = films;
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
+}
